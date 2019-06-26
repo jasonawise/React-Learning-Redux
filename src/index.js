e136d33a9ab5ec86b5eb7b651f26910fae7605a4
jasonwise <jasonandrewwise@gmail.com>
2019-06-26 13:23:35 -0400

    providing store to the app

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './components/App';
+import store from './redux/store';
 
-const title = 'My React Boilerplate';
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
 
-ReactDOM.render(<div>{title}</div>, document.getElementById('app'));
+  document.getElementById('app')
+);
